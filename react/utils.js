8278fc5747c14c3c68a9f1a2af0991768ccf51a2
Bishal Biswas <bishalbiswas@FC-BishalBiswas.local>
2020-11-22 16:24:30 +0530

    fix(utils.js): context saving functionality

--- react/utils.js
+++ react/utils.js
@@ -1,17 +1,15 @@
-export function getReactTree(elem, args) {
-    
-    // let key = 'root';
-    // if(con && con.key) {
-    //     key = con.key;
-    // }
-    // console.log("P: ", key);
-
-    let dom = null;
-    if(!dom) {
-        dom = new elem(args);
-    }
-
-    return (new elem(args)).getJSX()
+const baseRef = {}
+export function getReactTree(elem, args, parent) {
+    const parentContext = parent || baseRef // if no parent then take baseRef
+    const childPosition = (parent && parentContext.__childSequence) || 0
+    const uniqueKey = elem.name + '__' + childPosition
+    const comp = (parentContext.__childrens && parentContext.__childrens[uniqueKey]) || new elem(args)
+    if (!parentContext.__childrens) parentContext.__childrens = {}
+    parentContext.__childrens[uniqueKey] = comp
+    comp.__childSequence = 0 // reset for proper rendering of comp childs
+    parentContext.__childSequence = childPosition + 1;
+    console.log("Context", parentContext)
+    return comp.getJSX()
 }
 
 // export function getJSXwithNewState(elem, args) {
